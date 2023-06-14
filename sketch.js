41c0b965f7d02dc9c2a502a084f91985c88a6a57
xdKya <90745676+xdKya@users.noreply.github.com>
2023-06-14 11:15:24 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function preload() {}
+
+function setup() {
+  createCanvas(1000, 800);
+}
+
+function draw() {
+  background("white");
+
+  text("Seu jogo aqui :D", width / 2, height / 2);
+
+  drawSprites();
+}
